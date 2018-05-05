5786ca8d3087b034ac51099bacd8ffb405feab3d
Hcreak <hcreak@outlook.com>
2018-05-05 23:30:44 +0800

    disk BOOM! debug

--- backup_script.sh
+++ backup_script.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ ! -d /backup ]; then
+	mkdir /backup
+fi
+
 if [ $(date +%d) -eq 01 ];then
 	rm -rf /backup/snapshot.snar
 
@@ -7,6 +11,9 @@ if [ $(date +%d) -eq 01 ];then
 	do
 		rm -rf $file
 	done
+
+	##disk BOOM! debug:only 5 months (150 days)
+	find /backup -mtime +150 -type f -name '*.tar.gz' -exec rm -rf {} \;	
 fi
 
 tar -czpf /backup/backup_$(date +%Y%m%d).tar.gz -g /backup/snapshot.snar --exclude=/backup --exclude=/proc --exclude=/lost+found --exclude=/sys --exclude=/mnt --exclude=/media --exclude=/dev --exclude=/tmp /  > /dev/null 2>&1
